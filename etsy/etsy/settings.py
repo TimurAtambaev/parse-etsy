276c98964bac23fc488f7752956e85d17fa0ed7f
Andrey Shahov <andrey.shahov@webpp.ru>
2022-01-14 18:08:48 +0300

    Настройка сборки

--- etsy/etsy/settings.py
+++ etsy/etsy/settings.py
@@ -78,9 +78,14 @@ WSGI_APPLICATION = 'etsy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        'PORT': os.environ.get('POSTGRES_PORT', 5432),
     }
+
 }
 
 
