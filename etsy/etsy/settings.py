6a1fddd07dd19f158aebeed57563a81303002ee2
Тимур Атамбаев <timur.atambaev@webpp.ru>
2022-01-28 15:30:28 +0300

    PARSE-1 Поправлено расположение media папки

--- etsy/etsy/settings.py
+++ etsy/etsy/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'etsy/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
